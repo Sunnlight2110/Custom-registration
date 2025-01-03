5d27af5e1210233d8dc245ae77f0c8ea03306a23
Sunlight2110 <hmore7978@gmail.com>
2025-01-03 12:14:29 +0530

    solved get_vy_natural_key

--- registration/models.py
+++ registration/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+class ProjectManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(email, first_name, last_name, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    def get_by_natural_key(self, email):
+        return self.get(email=email)
+    
 class StudentModel(AbstractBaseUser):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -11,7 +30,8 @@ class StudentModel(AbstractBaseUser):
 
 
     USERNAME_FIELD = 'email' 
-    REQUIRED_FIELDS = ['first_name', 'last_name'] 
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = ProjectManager()
 
     def __str__(self):
         return self.email
@@ -25,5 +45,5 @@ class StudentModel(AbstractBaseUser):
         """ Check password with stored hashed password """
         from django.contrib.auth.hashers import check_password
         return check_password(raw_password, self.password)
-    
+
 
