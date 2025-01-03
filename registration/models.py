9c476aa64a57d45586880b7345b44d6c5eec8e3f
Sunlight2110 <hmore7978@gmail.com>
2025-01-03 11:07:48 +0530

    a

--- registration/models.py
+++ registration/models.py
@@ -1,7 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import BaseUserManager
 
-# Custom User Model without StudentManager
+class StudentManager(BaseUserManager):
+    """
+    Custom manager for the StudentModel.
+    """
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field is required.")
+        if not first_name:
+            raise ValueError("The First Name field is required.")
+        if not last_name:
+            raise ValueError("The Last Name field is required.")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)  # Save to the database
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+
+
 class StudentModel(AbstractBaseUser):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -26,6 +57,4 @@ class StudentModel(AbstractBaseUser):
         from django.contrib.auth.hashers import check_password
         return check_password(raw_password, self.password)
     
-    # def get_by_natural_key(self, email):
-    #     return self.get(email=email)
 
