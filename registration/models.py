c3802fea2a017ea4d1778707c043788a42c38722
Sunlight2110 <hmore7978@gmail.com>
2025-01-03 11:08:24 +0530

    a

--- registration/models.py
+++ registration/models.py
@@ -1,36 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import BaseUserManager
-
-class StudentManager(BaseUserManager):
-    """
-    Custom manager for the StudentModel.
-    """
-    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field is required.")
-        if not first_name:
-            raise ValueError("The First Name field is required.")
-        if not last_name:
-            raise ValueError("The Last Name field is required.")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
-        user.set_password(password)  # Hash the password
-        user.save(using=self._db)  # Save to the database
-        return user
-
-    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, first_name, last_name, password, **extra_fields)
-
 
 
 class StudentModel(AbstractBaseUser):
